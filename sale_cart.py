a4c9300dce395cf690afaf448015570e19a48aff
resteve <resteve@zikzakmedia.com>
2015-08-26 17:18:29 +0200

    New API for on_change

--- sale_cart.py
+++ sale_cart.py
@@ -3,6 +3,7 @@
 # the full copyright notices and license terms.
 from decimal import Decimal
 from trytond.pool import Pool, PoolMeta
+from trytond.model import fields
 from trytond.transaction import Transaction
 
 __all__ = ['SaleCart']
@@ -12,23 +13,39 @@ __metaclass__ = PoolMeta
 class SaleCart:
     __name__ = 'sale.cart'
 
-    def update_prices(self):
-        if hasattr(self, 'product'):
-            Product = Pool().get('product.product')
+    def update_prices_visible_discount(self):
+        Product = Pool().get('product.product')
 
-            with Transaction().set_context(without_special_price=True):
-                self.discount = Decimal(0)
-                res = super(SaleCart, self).update_prices()
-                gross_unit_price = Product.get_sale_price([self.product],
-                        self.quantity or 0)[self.product.id]
-                if gross_unit_price:
-                    unit_price_digits = self.__class__.gross_unit_price.digits[1]
-                    discount_digits = self.__class__.discount.digits[1]
-                    res['gross_unit_price'] = gross_unit_price.quantize(
-                        Decimal(str(10.0 ** -unit_price_digits)))
-                    discount = 1 - (res['unit_price'] / res['gross_unit_price'])
-                    res['discount'] = discount.quantize(
-                        Decimal(str(10.0 ** -discount_digits)))
-        else:
-            res = super(SaleCart, self).update_prices()
-        return res
+        unit_price = self.gross_unit_price
+        discount = Decimal(0)
+
+        with Transaction().set_context(without_special_price=True):
+            prices = Product.get_sale_price([self.product], self.quantity or 0)
+        gross_unit_price = prices[self.product.id]
+
+        if gross_unit_price:
+            unit_price_digits = self.__class__.gross_unit_price.digits[1]
+            discount_digits = self.__class__.discount.digits[1]
+            gross_unit_price = gross_unit_price.quantize(
+                Decimal(str(10.0 ** -unit_price_digits)))
+            discount = 1 - (self.unit_price / gross_unit_price)
+            discount = discount.quantize(
+                Decimal(str(10.0 ** -discount_digits)))
+
+        self.gross_unit_price = gross_unit_price
+        self.discount = discount
+        self.unit_price = unit_price
+
+    @fields.depends('gross_unit_price')
+    def on_change_product(self):
+        super(SaleCart, self).on_change_product()
+        if not self.product:
+            return
+        if self.gross_unit_price:
+            self.update_prices_visible_discount()
+
+    @fields.depends('gross_unit_price')
+    def on_change_quantity(self):
+        super(SaleCart, self).on_change_quantity()
+        if self.gross_unit_price:
+            self.update_prices_visible_discount()
