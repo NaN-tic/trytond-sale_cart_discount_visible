eea85298f12fb2bd107213dbc1fa60c0d2aa1dea
resteve <resteve@zikzakmedia.com>
2015-11-29 23:29:50 +0100

    Tests

--- tests/test_sale_cart_discount_visible.py
+++ tests/test_sale_cart_discount_visible.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
-# This file is part sale_cart_discount_visible module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the sale_cart_discount_visible module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleCartDiscountVisibleTestCase(unittest.TestCase):
+class SaleCartDiscountVisibleTestCase(ModuleTestCase):
     'Test Sale Cart Discount Visible module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_cart_discount_visible')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_cart_discount_visible'
 
 
 def suite():
@@ -23,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         SaleCartDiscountVisibleTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
